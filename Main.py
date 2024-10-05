0a9403e61ab9303c3793e05c207e3a98eb3b1e6b
Sai Siddharth <101695055+Sacrobots@users.noreply.github.com>
2024-10-05 23:45:53 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,8 +1,7 @@
 from ML_Model import *
 from ReadClean import *
 import streamlit as st
-
-
+from Original_CSV_File import presentData
 
 
 first = readData()
@@ -24,11 +23,17 @@ def page2():
     st.sidebar.markdown("**Original CSV Charts Representation 📊**")
     Graphs(first)
 
+
+def page3():
+    st.title("**Dataset Presented Below**")
+    st.sidebar.markdown("**Contents Of CSV 📋**")
+    presentData()
     
 
 page_names_to_funcs = {
     "Insurance Price Predicator 🔰": main_page,
-    "Graphical Representation": page2
+    "Graphical Representation": page2,
+    "Contents Of Original CSV File": page3
 }
 
 selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
